4ebc6425732beff283b2afb379cc2da84ee9c9e9
g3ry89 <gery89@gmail.com>
2019-02-20 08:12:43 +0100

    basic ks eureka-server structure implemented, but due to missing dependencies, not working yet

---
+++ ks-eureka/src/main/java/com/ks/eurekaserver/KulcsSoftEurekaServer.java
@@ -0,0 +1,13 @@
+package com.ks.eurekaserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/*@EnableEurekaServer*/
+public class KulcsSoftEurekaServer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
