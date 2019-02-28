06386c14b05b2fd8fb595dc4dba5972d0d476d9a
g3ry89 <gery89@gmail.com>
2019-02-28 15:50:17 +0100

    db connection details has been removed from application.properties

--- ks-userservice/src/main/java/com/ks/userservice/security/APISecurityConfig.java
+++ ks-userservice/src/main/java/com/ks/userservice/security/APISecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
